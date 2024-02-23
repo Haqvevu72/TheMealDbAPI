0d1c71a70e68206bcd21529c85770c819bfe1cc7
Elgun Haqverdiyev <elgunhaqverdiyev@Elguns-MacBook-Air.local>
2024-02-23 15:11:58 +0400

    Commit1

---
+++ script/index.js
@@ -0,0 +1,37 @@
+
+// Task 1 script ...
+
+// const mealName = document.getElementById('mealName');
+// const foodImage = document.getElementById('foodImage');
+
+// async function createRequest()
+// {
+//     const object = await fetch("https:www.themealdb.com/api/json/v1/1/random.php")
+//     .then(respone => respone.json())
+//     .then(data => data.meals[0]);
+
+
+//     mealName.innerHTML = object.strMeal;
+//     foodImage.src = object.strMealThumb;
+// }
+
+// Task 2 script ...
+// const mealName = document.getElementById('mealName');
+// const mealtext = document.getElementById('mealtext');
+// const foodImage = document.getElementById('foodImage');
+
+// async function createRequest()
+// {
+//     const object = await fetch(`https:www.themealdb.com/api/json/v1/1/search.php?s=${mealName.value}`)
+//     .then(respone => respone.json())
+//     .then(data => data.meals[0])
+//     .catch(message => console.log("Whoops ..."));
+
+
+//     mealtext.innerHTML = object.strMeal;
+//     foodImage.src = object.strMealThumb;
+// }
+
+
+
+        
